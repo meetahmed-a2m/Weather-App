010a57d3f387029e4e0d123539337c910d81818a
meetahmed-a2m <devineprogrammer91@gmail.com>
2026-01-20 18:46:26 +0500

    Updated with .Catch

--- Weather-app.js
+++ Weather-app.js
@@ -12,18 +12,22 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInfo}&appid=771fa
 
 .then((packedData)=> 
 {
-return packedData.json();
+   if(!packedData.ok)
+    {
+       throw new Error("No Valid City Name Found")
+    }
+     return packedData.json();
 })
-.then((CityData)=>
+    .then((CityData)=>
     {      
+ 
         const weatherInfo= document.querySelector("#weatherInfo") 
         weatherInfo.innerHTML= `<br> ${cityInfo} 🌡️ temperature is ${CityData.main.temp} °C <br> But it feels like ${CityData.main.feels_like} °C 
         <br> Wind Speed 🍃: ${CityData.wind.speed} <br> Weather ☁️: ${CityData.weather[0].description}  <br> Humidity Status: ${CityData.main.humidity} 
         <br> Country 🌍: ${CityData.sys.country} <br><br>Thank you for checking 😊`
-   }
-    
-)
+   })
 
+    .catch(errorInfo =>  weatherInfo.innerHTML= `<br>${errorInfo.message}`)
 
 
 }
