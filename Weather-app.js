fda7c2dcc576eb6e3cd9defbc98b12111a92068d
meetahmed-a2m <devineprogrammer91@gmail.com>
2026-01-23 01:08:10 +0500

    updated with icon

--- Weather-app.js
+++ Weather-app.js
@@ -8,7 +8,7 @@ function weatherFunction()
 const cityInfo = document.querySelector("#cityname").value
 // const cityInfo = cityInfoBox.value;
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInfo}&appid=771fa99984f7f60bcda45e1ae02c85c9&units=metric`)
+fetch(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${cityInfo}`)
 
 .then((packedData)=> 
 {
@@ -20,12 +20,20 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInfo}&appid=771fa
 })
     .then((CityData)=>
     {      
- 
+        console.log(CityData);       
+                 
+        const iconUrl = "https:" + CityData.current.condition.icon; 
+
+      document.getElementById("weatherIcon").src = iconUrl;
+
         const weatherInfo= document.querySelector("#weatherInfo") 
-        weatherInfo.innerHTML= `<br> ${cityInfo} 🌡️ temperature is ${CityData.main.temp} °C <br> But it feels like ${CityData.main.feels_like} °C 
-        <br> Wind Speed 🍃: ${CityData.wind.speed} <br> Weather ☁️: ${CityData.weather[0].description}  <br> Humidity Status: ${CityData.main.humidity} 
-        <br> Country 🌍: ${CityData.sys.country} <br><br>Thank you for checking 😊`
-   })
+        weatherInfo.innerHTML= `<br>🌡️ Temperature in ${cityInfo} is ${CityData.current.temp_c} °C but it feels like ${CityData.current.feelslike_c} °C <br>🌡️ Fahrenheit: ${CityData.current.temp_f} °F
+        <br> 🍃 Wind Speed : ${CityData.current.wind_mph} <br> ☁️ Weather: ${CityData.current.condition.text}  <br> 💧 Humidity Status: ${CityData.current.humidity}% 
+        <br> 🌍 Country: ${CityData.location.region}, ${CityData.location.country} <br> ⏰ Current Time: ${CityData.location.localtime} <br><br>Thank you for checking 😊`
+
+
+
+    })
 
     .catch(errorInfo =>  weatherInfo.innerHTML= `<br>${errorInfo.message}`)
 
